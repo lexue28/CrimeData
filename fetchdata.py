83c852a6f7b54848f6ab235ac9f46be8060ae5fa
frogg5 <75817943+frogg5@users.noreply.github.com>
2022-05-26 09:43:32 -0400

    Create fetchdata.py

---
+++ fetchdata.py
@@ -0,0 +1,8 @@
+import csv
+import numpy as np
+
+def fetchZipCode(): #and etc, more functions for cities, region, etc.
+  pass
+def fetchCrimeStastic():
+  pass
+  #needs to be multiple functions this is a placeholder
