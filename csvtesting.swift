d4c1298442682e6bdd18ee3cb1410678d6ddfa09
frogg5 <75817943+frogg5@users.noreply.github.com>
2022-08-02 14:47:39 -0400

    Update csvtesting.swift

--- csvtesting.swift
+++ csvtesting.swift
@@ -1,18 +1,11 @@
-/*do {
-     let csvData = try String(contentsOfFile: "file")
-     let csv = csvData.csvRows()
-     let str = ""
-     for row in csv {
-          let array = row.components(separatedBy: ",")
-          print(array)
-     }
-} catch {
-    print(error)
-}
-*/
+public var uscities = "/docs/csv/uscities.csv"
+public var crimedata = "/docs/csv/crime_data_w_population_and_crime_rate.csv"
+public var citieszip = "/docs/csv/report.csv"
+
+//input city name string, returns zip code string
 func getCityGivenZip (zipinp: String) -> String {
   do {
-       let csvData = try String(contentsOfFile: "file")
+       let csvData = try String(contentsOfFile: citieszip)
        let csv = csvData.csvRows()
        let str = ""
        for row in csv {
@@ -28,9 +21,10 @@ func getCityGivenZip (zipinp: String) -> String {
   }
 }
 
+//input zip code string, returns city string
 func getZipGivenCity (cityInp: String) -> String {
   do {
-       let csvData = try String(contentsOfFile: "file")
+       let csvData = try String(contentsOfFile: citieszip)
        let csv = csvData.csvRows()
        let str = ""
        for row in csv {
@@ -46,12 +40,12 @@ func getZipGivenCity (cityInp: String) -> String {
   }
 }
 
-
+/*
 
 func findC (countyname, statename : String) -> String {
   do {
 
-       let csvData = try String(contentsOfFile: "/docs/csv/uscities.csv")
+       let csvData = try String(contentsOfFile: uscities)
        let csv = csvData.csvRows()
        let arr = []
        for row in csv {
@@ -68,6 +62,40 @@ func findC (countyname, statename : String) -> String {
             }
        }
   } catch {
-      print(error) */
+      print(error)
   }
 }
+
+*/
+
+
+//function to recall crime states. find specific one by calling self.county_name, self.crime_rate_per_100000, self.MURDER, etc.
+func returncrimestats(countyname: String) -> (county_name: String, crime_rate_per_100000: Float, index: String, EDITION: Int, Part: Int, IDNO: Int, CPOPARST: Int, AG_ARRST: Int, AG_OFF: Int, COVIND: Int, INDEX: Int, MODINDX: Int, MURDER: Int, RAPE: Int, ROBBERY: Int, AGASSLT: Int, BURGLRY: Int, LARCENY: Int, MVTHEFT: Int, ARSON: Int, population: Int, FIPS_ST: Int, FIPS_CITY: Int) { //enter countyname
+    let csvData = try String(contentsOfFile: citieszip)
+    let csv = csvData.csvRows()
+    let arrayofcrimes = []
+    for row in csv {
+         let array = row.components(separatedBy: ",")
+         if (String(array[0]).lowercased() == countyname.lowercased()){
+             Swift.print(String(array))
+             arrayofcrimes = array
+             break
+         }
+         else{
+           Swift.print("county not found, please check spelling")
+         }
+
+         Swift.print(arrayofcrimes)
+         return(arrayofcrimes[0],arrayofcrimes[1],arrayofcrimes[2],arrayofcrimes[3],arrayofcrimes[4],arrayofcrimes[5],arrayofcrimes[6],arrayofcrimes[7],arrayofcrimes[8],arrayofcrimes[9],arrayofcrimes[10],arrayofcrimes[11],arrayofcrimes[12],arrayofcrimes[13],arrayofcrimes[14],arrayofcrimes[15],arrayofcrimes[16],arrayofcrimes[17],arrayofcrimes[18],arrayofcrimes[19],arrayofcrimes[20],arrayofcrimes[21],arrayofcrimes[22],arrayofcrimes[23])
+
+      //   categories are
+      //   county_name,crime_rate_per_100000,index,EDITION,PART,IDNO,CPOPARST,CPOPCRIM,AG_ARRST,AG_OFF,COVIND,INDEX,MODINDX,MURDER,RAPE,ROBBERY,AGASSLT,BURGLRY,LARCENY,MVTHEFT,ARSON,population,FIPS_ST,FIPS_CTY
+
+}
+
+/*
+func mean() -> () {
+
+
+}
+*/
